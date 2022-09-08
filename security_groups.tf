1ac3daf5b03429e76e1c87ec688a03d2328743a3
astanil <astakhovnil@gmail.com>
2022-09-08 18:42:16 +0300

    Added security group

---
+++ security_groups.tf
@@ -0,0 +1,21 @@
+resource "azurerm_network_security_group" "netframe_sg" {
+  name                = "netframe-sg"
+  location            = azurerm_resource_group.netframe_rg.location
+  resource_group_name = azurerm_resource_group.netframe_rg.name
+
+  security_rule {
+    name                       = "ssh"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "22"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = "Production"
+  }
+}
