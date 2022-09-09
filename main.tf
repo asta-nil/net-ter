0eef7135890285ecd63a530c7651b1985a212650
astanil <astakhovnil@gmail.com>
2022-09-09 13:28:03 +0300

    Edited security group

--- main.tf
+++ main.tf
@@ -29,6 +29,26 @@ resource "azurerm_network_interface" "netframe_nic" {
   }
 }
 
+resource "azurerm_network_security_group" "netframe_sg" {
+  name                = "netframe-sg"
+  location            = azurerm_resource_group.netframe_rg.location
+  resource_group_name = azurerm_resource_group.netframe_rg.name
+}
+
+resource "azurerm_network_security_rule" "allow-ssh" {
+  name                       = "SSH"
+  priority                   = 1001
+  direction                  = "Inbound"
+  access                     = "Allow"
+  protocol                   = "Tcp"
+  source_port_range          = "*"
+  destination_port_range     = "22"
+  source_address_prefix      = "*"
+  destination_address_prefix = "*"
+  resource_group_name         = azurerm_resource_group.netframe_rg.name
+  network_security_group_name = azurerm_network_security_group.netframe_sg.name
+}
+
 resource "azurerm_virtual_machine" "netframe_vm" {
   name                  = "${var.prefix}-vm"
   location              = azurerm_resource_group.netframe_rg.location
