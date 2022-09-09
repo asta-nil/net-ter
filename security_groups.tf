c5eb70fef8fc55ab350e29e00948b10698b429cb
astanil <astakhovnil@gmail.com>
2022-09-09 12:38:16 +0300

    Edited security group

--- security_groups.tf
+++ security_groups.tf
@@ -5,17 +5,15 @@ resource "azurerm_network_security_group" "netframe_sg" {
 }
 
 resource "azurerm_network_security_rule" "allow-ssh" {
-  security_rule {
-    name                       = "SSH"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-    resource_group_name         = azurerm_resource_group.netframe_rg.name
-    network_security_group_name = azurerm_network_security_group.example.name
-  }
+  name                       = "SSH"
+  priority                   = 1001
+  direction                  = "Inbound"
+  access                     = "Allow"
+  protocol                   = "Tcp"
+  source_port_range          = "*"
+  destination_port_range     = "22"
+  source_address_prefix      = "*"
+  destination_address_prefix = "*"
+  resource_group_name         = azurerm_resource_group.netframe_rg.name
+  network_security_group_name = azurerm_network_security_group.example.name
 }
