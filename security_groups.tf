9a505bdac3ff4dfc413eda9d7ce3066a58364558
astanil <astakhovnil@gmail.com>
2022-09-09 12:39:19 +0300

    Edited security group

--- security_groups.tf
+++ security_groups.tf
@@ -15,5 +15,5 @@ resource "azurerm_network_security_rule" "allow-ssh" {
   source_address_prefix      = "*"
   destination_address_prefix = "*"
   resource_group_name         = azurerm_resource_group.netframe_rg.name
-  network_security_group_name = azurerm_network_security_group.example.name
+  network_security_group_name = azurerm_network_security_group.netframe_sg.name
 }
