fe3e4f3db3c34920ae7062a1b7d92211252ce41a
astanil <astakhovnil@gmail.com>
2022-09-09 14:07:27 +0300

    Added public ip

--- main.tf
+++ main.tf
@@ -17,15 +17,27 @@ resource "azurerm_subnet" "internal" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
+resource "azurerm_public_ip" "netframe_ip" {
+  name                = "${var.prefix}-ip"
+  resource_group_name = azurerm_resource_group.netframe_rg.name
+  location            = azurerm_resource_group.netframe_rg.location
+  allocation_method   = "Dynamic"
+
+  tags = {
+    environment = "Production"
+  }
+}
+
 resource "azurerm_network_interface" "netframe_nic" {
   name                = "${var.prefix}-nic"
   location            = azurerm_resource_group.netframe_rg.location
   resource_group_name = azurerm_resource_group.netframe_rg.name
 
   ip_configuration {
-    name                          = "testconfiguration1"
+    name                          = "private-ip-config"
     subnet_id                     = azurerm_subnet.internal.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.netframe_ip.id
   }
 }
 
@@ -63,16 +75,5 @@ resource "azurerm_virtual_machine" "netframe_vm" {
   }
   tags = {
     environment = "staging"
-  }
-}
-
-resource "azurerm_public_ip" "netframe_ip" {
-  name                = "${var.prefix}-ip"
-  resource_group_name = azurerm_resource_group.netframe_rg.name
-  location            = azurerm_resource_group.netframe_rg.location
-  allocation_method   = "Static"
-
-  tags = {
-    environment = "Production"
   }
 }
