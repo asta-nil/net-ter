77f56c9dde8fa8baca6fb943ccf0b08ef027f5b7
astanil <astakhovnil@gmail.com>
2022-09-12 15:40:42 +0300

    reformated code

--- providers.tf
+++ providers.tf
@@ -5,12 +5,6 @@ terraform {
       version = "=3.0.1"
     }
   }
-  backend "azurerm" {
-    resource_group_name     = "cloud-shell-storage-westeurope"
-    storage_account_name    = "csb100320022982d2df"
-    container_name          = "tfstate"
-    key                     = "hello-world"
-  }
 }
 
 provider "azurerm" {
