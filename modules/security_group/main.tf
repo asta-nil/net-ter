c9faf889b61e9a04926e28c6cea7adee77114bc5
astanil <astakhovnil@gmail.com>
2022-09-12 16:37:40 +0300

    reformated code

--- modules/security_group/main.tf
+++ modules/security_group/main.tf
@@ -17,4 +17,3 @@ resource "azurerm_network_security_rule" "allow-ssh" {
   resource_group_name         = azurerm_resource_group.netframe_rg.name
   network_security_group_name = azurerm_network_security_group.netframe_sg.name
 }
-
