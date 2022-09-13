1d966f623918f891a070a374fe29c6bc92209b1c
astanil <astakhovnil@gmail.com>
2022-09-13 11:23:41 +0300

    refactoring security group

--- security_group.tf
+++ security_group.tf
@@ -1,20 +1,5 @@
-resource "azurerm_network_security_group" "netframe_sg" {
-  name                = "netframe-sg"
-  location            = azurerm_resource_group.netframe_rg.location
+module "security_group" {
+  source = "./modules/security_group"
   resource_group_name = azurerm_resource_group.netframe_rg.name
+  location = "West Europe"
 }
-
-resource "azurerm_network_security_rule" "allow-ssh" {
-  name                       = "SSH"
-  priority                   = 1001
-  direction                  = "Inbound"
-  access                     = "Allow"
-  protocol                   = "Tcp"
-  source_port_range          = "*"
-  destination_port_range     = "22"
-  source_address_prefix      = "*"
-  destination_address_prefix = "*"
-  resource_group_name         = azurerm_resource_group.netframe_rg.name
-  network_security_group_name = azurerm_network_security_group.netframe_sg.name
-}
-
