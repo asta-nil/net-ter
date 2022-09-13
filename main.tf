ee62f020dcb1b7e0a873926191941738affbfac8
astanil <astakhovnil@gmail.com>
2022-09-13 13:17:59 +0300

    refactored code

--- main.tf
+++ main.tf
@@ -51,7 +51,7 @@ resource "azurerm_virtual_machine" "netframe_vm" {
   name                  = "${var.prefix}-vm"
   location              = azurerm_resource_group.netframe_rg.location
   resource_group_name   = azurerm_resource_group.netframe_rg.name
-  network_interface_ids = [azurerm_network_interface.netframe_nic.id] 
+  network_interface_ids = [module.network_interface.network_interface_id]
   vm_size               = "Standard_DS1_v2"
 
   storage_image_reference {
