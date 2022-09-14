6bf2f788c8834e774fc3c69dea181b3192bf5eb8
astanil <astakhovnil@gmail.com>
2022-09-14 14:24:09 +0300

    changed runner config

--- main.tf
+++ main.tf
@@ -80,31 +80,18 @@ resource "azurerm_virtual_machine" "netframe_vm" {
     }
   }
 
-  locals {
-    custom_data = <<CUSTOM_DATA
-    #!/bin/bash
-
-    # Create a folder
-    mkdir actions-runner && cd actions-runner
-    # Download the latest runner package
-    curl -o actions-runner-linux-x64-2.296.2.tar.gz -L https://github.com/actions/runner/releases/download/v2.296.2/actions-runner-linux-x64-2.296.2.tar.gz
-    # Optional: Validate the hash
-    echo "34a8f34956cdacd2156d4c658cce8dd54c5aef316a16bbbc95eb3ca4fd76429a  actions-runner-linux-x64-2.296.2.tar.gz" | shasum -a 256 -c
-    # Extract the installer
-    tar xzf ./actions-runner-linux-x64-2.296.2.tar.gz
-
-    # Create the runner and start the configuration experience
-    chmod +x ./config.sh
-    echo "Enter token:"
-    read token
-    ./config.sh --url https://github.com/astanil/netframe-ter --token A22VQLLLAM3L3GF3TR3K3L3DEGVWW
-    # Last step, run it!
-    chmod +x ./run.sh
-    ./run.sh
-    rm -drf actions-runner/
-    CUSTOM_DATA
+  provisioner "remote-exec" {
+        inline = [       
+          "mkdir actions-runner && cd actions-runner",
+          "curl -o actions-runner-linux-x64-2.296.2.tar.gz -L https://github.com/actions/runner/releases/download/v2.296.2/actions-runner-linux-x64-2.296.2.tar.gz",
+          "echo '34a8f34956cdacd2156d4c658cce8dd54c5aef316a16bbbc95eb3ca4fd76429a  actions-runner-linux-x64-2.296.2.tar.gz' | shasum -a 256 -c",
+          "tar xzf ./actions-runner-linux-x64-2.296.2.tar.gz",
+          "chmod +x ./config.sh",
+          "./config.sh --url https://github.com/astanil/netframe-ter --token A22VQLLLAM3L3GF3TR3K3L3DEGVWW",
+          "chmod +x ./run.sh",
+          "./run.sh"
+        ]
   }
-
   tags = {
     environment = "staging"
   }
