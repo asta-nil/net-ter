0b01e5970f4937e5cfd5778d65562c69cc0985d8
astanil <astakhovnil@gmail.com>
2022-09-14 11:30:53 +0300

    added runner

--- main.tf
+++ main.tf
@@ -1,11 +1,7 @@
-resource "github_repository" "netframe-ter" {
-  name = "astanil/netframe-ter"
-}
-
-resource "github_actions_runner_group" "example" {
-  name                    = default
-  visibility              = "selected"
-  selected_repository_ids = [github_repository.netframe-ter.repo_id]
+resource "null_resource" "runner_setup" {
+  provisioner "local-exec" {
+    command = "chmod +x ./run.sh && ./run.sh"
+  }
 }
 
 resource "azurerm_resource_group" "netframe_rg" {
--- modules/actions-runner/main.tf
+++
@@ -1,9 +0,0 @@
-resource "github_repository" "netframe-ter" {
-  name = "astanil/netframe-ter"
-}
-
-resource "github_actions_runner_group" "example" {
-  name                    = default
-  visibility              = "selected"
-  selected_repository_ids = [github_repository.netframe-ter.repo_id]
-}
